25be5787f1dd48d8325878bdb34834adf6e06e2c
Jason Edgerunner <brandonversteeg218@hotmail.com>
2022-10-13 14:15:38 +0200

    Added functions to template

---
+++ HaikuThingy.py
@@ -0,0 +1,11 @@
+
+
+#plak hier de haiku functions onder elkaar
+def HaikuX(): print("haiku by:","AUTHOR") print("REGEL1") print("REGEL2") print("REGEL3")
+def HaikuX(): print("haiku by:","AUTHOR") print("REGEL1") print("REGEL2") print("REGEL3")
+def HaikuX(): print("haiku by:","AUTHOR") print("REGEL1") print("REGEL2") print("REGEL3")
+
+#laat de code hieronder met rust
+Haiku1()
+Haiku2()
+Haiku3()
